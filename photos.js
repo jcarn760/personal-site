161a5fdd617323b02aa9e38ec1b7866e0897d7ae
Josh <53236828+jcarn760@users.noreply.github.com>
2023-05-03 16:41:49 -0400

    updates to change where photoshop images direct

---
+++ photos.js
@@ -0,0 +1,26 @@
+
+// var moviePoster = "./images/photoshop/moviePoster.jpg";
+// var changeHistory = "./images/photoshop/changeHistory.jpg";
+// var motionBlur = "./images/photoshop/motionBlur.jpg";
+// var artMagazine = "./images/photoshop/artMagazineCover.jpg";
+
+// function storeId(clicked_id) {
+//     var newImage = clicked_id;
+//     return newImage;
+// }
+
+// function loadImage() {
+//     if(newImage == "moviePoster"){
+//         document.getElementsByClassName("PSimage").src = "./images/photoshop/moviePoster.jpg";
+//     } else if (newImage == "changeHistory"){
+//         document.getElementsByClassName("PSimage").src = "./images/photoshop/changeHistory.jpg";
+//     } else if (newImage == "motionBlur"){
+//         document.getElementsByClassName("PSimage").src = "./images/photoshop/motionBlur.jpg";
+//     } else if (newImage == "artMagazine"){
+//         document.getElementsByClassName("PSimage").src = "./images/photoshop/artMagazineCover.jpg";
+//     }
+// }
+
+// console.log(newImage);
+
+
